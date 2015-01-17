ab4e8eeaaf614c7cdf4388246dcb0d6d2d59b556
Meir Shahar <meir.shahar@gmail.com>
2015-01-17 22:34:38 +0200

    added instruction to the init method

--- imageProcessingInit.m
+++ imageProcessingInit.m
@@ -1,2 +1,10 @@
+%{
+init function for the Israeli study group
+run this when you start matlab to have all sub folders available
+to add your script:
+1. Createa a sub folder with your name (example: adi)
+2. Add an addpath command with your folder below: addpath('adi')
+3. Save the file, git commit and push
+%}
 function imageProcessingInit()
 addpath('meir');
